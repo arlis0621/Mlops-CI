0aa33430e5ef40c2c02446d6b93b9ff6c38c4fbe
Aditya <mt1221953@maths.iitd.ac.in>
2025-03-20 13:59:56 +0530

    first commit

---
+++ github/workflows/test_1.py
@@ -0,0 +1,39 @@
+import pytest
+
+def square(n):
+    return n ** 2
+
+def cube(n):
+    return n ** 3
+
+
+
+
+
+
+
+
+
+
+def fifth_power(n):
+    return n ** 5
+
+def test_square():
+    assert square(2) == 4, "Test Failed: Square of 2 is 4"  
+    assert square(3) == 9, "Test Failed: Square of 3 is 9"  
+
+def test_cube():
+    assert cube(2) == 8, "Test Failed: Cube of 2 is 8"  
+    assert cube(3) == 27, "Test Failed: Cube of 3 is 27"
+
+def test_fifth_power():
+    assert fifth_power(2) == 32, "Test Failed: Fifth power of 2 is 32"  
+    assert fifth_power(3) == 243, "Test Failed: Fifth power of 3 is 243"
+    
+    
+    
+def test_invalid_input():
+    with pytest.raises(TypeError):
+        square("hello")
+        cube("hello")
+    
